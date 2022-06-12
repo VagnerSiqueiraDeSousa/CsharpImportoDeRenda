d09cb01b9d76c1b33d90d62d3ab09fd5f30d9b62
VagnerSousa <107362281+VagnerSiqueiraDeSousa@users.noreply.github.com>
2022-06-12 18:52:29 -0300

    Add files via upload

---
+++ multiplosDeTres/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace multiplosDeTres
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                if(i % 3 == 0)
+                {
+                    Console.WriteLine(i);
+                   
+                }
+                
+            }
+            Console.WriteLine("nova execução com o For diferente");
+            for (int i = 2; i < 100; i += 2)
+            {
+                Console.WriteLine(i);
+            }
+            Console.ReadLine();
+        }
+    }
+    
+}
+    
+
