80bf3b3aad38155f2a2b0c47363ad7f41a8adabd
VagnerSousa <107362281+VagnerSiqueiraDeSousa@users.noreply.github.com>
2022-06-12 18:50:37 -0300

    Add files via upload

---
+++ imprimindoNunFatoriais/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace imprimindoNunFatoriais
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int fatorial = 1;
+            for (int contador = 1; contador < 11; contador++)
+            {
+                fatorial *= contador;
+                Console.Write("Fatorial de " + contador + " = " + fatorial);
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
